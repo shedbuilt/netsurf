8f3f467935101552fabcb4e67a7b790fa59a4851
Auston Stewart <auston@shedbuilt.net>
2018-03-12 11:45:17 -1000

    Fixing install location

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp -v "${SHED_CONTRIBDIR}/libparserutils/Makefile.config.override" libparserutils/ &&
 cp -v "${SHED_CONTRIBDIR}/netsurf/Makefile.config" netsurf/ &&
-make TARGET=framebuffer -j $SHED_NUMJOBS &&
-make TARGET=framebuffer DESTDIR="$SHED_FAKEROOT" install
+make TARGET=framebuffer PREFIX=/usr/local -j $SHED_NUMJOBS &&
+make TARGET=framebuffer PREFIX=/usr/local DESTDIR="$SHED_FAKEROOT" install
