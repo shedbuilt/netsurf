fc1e9fd49965d779f6d09c9d1e7609df2d2bafbb
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:03:06 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -v "${SHED_CONTRIBDIR}/libparserutils/Makefile.config.override" libparserutils/ &&
-cp -v "${SHED_CONTRIBDIR}/netsurf/Makefile.config" netsurf/ &&
-make TARGET=framebuffer PREFIX=/usr/local -j $SHED_NUMJOBS &&
-make TARGET=framebuffer PREFIX=/usr/local DESTDIR="$SHED_FAKEROOT" install
+cp -v "${SHED_PKG_CONTRIB_DIR}/libparserutils/Makefile.config.override" libparserutils/ &&
+cp -v "${SHED_PKG_CONTRIB_DIR}/netsurf/Makefile.config" netsurf/ &&
+make TARGET=framebuffer PREFIX=/usr/local -j $SHED_NUM_JOBS &&
+make TARGET=framebuffer PREFIX=/usr/local DESTDIR="$SHED_FAKE_ROOT" install
